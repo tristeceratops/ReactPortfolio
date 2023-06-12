c058ba81b800f9cf374419f52869df598c8a385d
Eric <eric.woillard@gmail.com>
2023-06-12 19:10:58 +0200

    bouton fleche

--- portfolio/src/pages/home.js
+++ portfolio/src/pages/home.js
@@ -1,3 +1,7 @@
+import "../presentation.css"
+import { FaArrowRight } from "react-icons/fa";
+import { FaArrowLeft } from "react-icons/fa";
+import { Link } from "react-router-dom";
 const Home = () => {
     return (
     <div>
@@ -11,10 +15,19 @@ const Home = () => {
       <section>
         <p>HEHO</p>
       </section>
+      <div class="fixed-button-left">
+      <Link to="/competence" className="round-button"><FaArrowLeft/></Link>
+      </div>
+      <div class="fixed-button-right">
+      <Link to="/competence" className="round-button"><FaArrowRight/></Link>
+      </div>
+
     </div>
  </div>
   )
   };
+
+
   
   export default Home;
   
--- portfolio/src/pages/presentation.js
+++ portfolio/src/pages/presentation.js
@@ -1,4 +1,7 @@
 import "../presentation.css"
+import { IconContext } from "react-icons";
+import { GiDiploma } from "react-icons/gi";
+import { AiOutlineSolution } from "react-icons/ai";
 
 const Presentation = () => {
     return( 
@@ -8,14 +11,32 @@ const Presentation = () => {
      <div className="square_grid">
       <div className = "square_wrapper">
           <div className="square_inner">
-            <div className =" recto"></div>
-            <div className =" verso"></div>
+            <div className =" recto">
+            <IconContext.Provider value={{ color: "black", className: "presentation_icon", size:"4em"}}>
+          <div className="icon_presentation_wrapper">
+            <AiOutlineSolution />
+          </div>
+        </IconContext.Provider>
+            </div>
+            <div className =" verso">
+              <p className="square_content">Nom : Woillard<br/>
+              Prénom : Éric <br/>
+              Profession : Développeur informatique (en devenir)</p>
+            </div>
           </div>
         </div>
         <div className = "square_wrapper">
           <div className="square_inner">
-            <div className =" recto"></div>
-            <div className =" verso"></div>
+            <div className =" recto"> 
+            <IconContext.Provider value={{ color: "black", className: "presentation_icon", size:"4em"}}>
+          <div className="icon_presentation_wrapper">
+            <GiDiploma />
+          </div>
+        </IconContext.Provider>
+            </div>
+            <div className =" verso">
+            <p className="square_content">DUT Informatique (Bac +2) : 2020-2022</p>
+            </div>
           </div>
         </div>
         <div className = "square_wrapper">
